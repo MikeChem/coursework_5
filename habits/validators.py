24df6543ad8284d7327f8181e460f3634c8cc500
Mikhail Lashukov <lashukovms@gmail.com>
2025-05-25 18:08:53 +0300

    fix readmy

---
+++ habits/validators.py
@@ -0,0 +1,11 @@
+from django.core.exceptions import ValidationError
+
+
+def validate_duration(value):
+    if value > 120:
+        raise ValidationError('Время выполнения не должно превышать 120 секунд.')
+
+
+def validate_frequency(value):
+    if value < 1 or value > 7:
+        raise ValidationError('Периодичность должна быть от 1 до 7 дней.')
